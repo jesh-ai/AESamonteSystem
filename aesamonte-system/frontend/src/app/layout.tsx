a5e31d00e20754b458a50da684c3787830a2e9a9
faialecz <faithaleczes27@gmail.com>
2026-01-20 03:08:40 +0800

    fixed logo in layout

--- aesamonte-system/frontend/src/app/layout.tsx
+++ aesamonte-system/frontend/src/app/layout.tsx
@@ -6,7 +6,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "AE Samonte Merchandise",
-  description: "An Inventory and Sales System for AE Samonte Merchandise ",
+  description: "An Inventory and Sales System for AE Samonte Merchandise",
+  icons: {
+    icon: "/ae-logo.png", // or "/logo.png" if that's your file name in the public folder
+  },
 };
 
 export default function RootLayout({
